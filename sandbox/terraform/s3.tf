8ca95fc02db51d447dc467562cf5b8d4fe36d2bd
Itay Shafir <itaysh@amazon.com>
2024-08-26 09:36:20 +0300

    First

--- sandbox/terraform/s3.tf
+++ sandbox/terraform/s3.tf
@@ -4,3 +4,12 @@ resource "aws_s3_bucket" "sandbox_bucket" {
   bucket = "aft-sandbox-${data.aws_caller_identity.current.account_id}"
   acl    = "private"
 }
+resource "aws_budgets_budget" "total_cost" {
+  name              = "budget-total-monthly"
+  budget_type       = "COST"
+  limit_amount      = "100"
+  limit_unit        = "USD"
+  time_period_end   = "2087-06-15_00:00"
+  time_period_start = "2022-02-01_00:00"
+  time_unit         = "MONTHLY"
+}
